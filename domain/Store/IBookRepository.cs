d19d11d1efba358feeb362e69fe40771590aa028
Violener <31727030+Violener@users.noreply.github.com>
2022-10-20 21:45:58 +0300

    zaebalo4

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Store;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace store
 {
-    interface IBookRepository
+   public interface IBookRepository
     {
+        Book[] GetAllByTitle(string titlepart);
+        
     }
 }
---
+++ infrastructure/Store.Memory/Class1.cs
@@ -0,0 +1,20 @@
+﻿using store;
+using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books =
+        {
+            new Book(1,"Art of Programming" ),
+            new Book(2,"Refactoring" ),
+            new Book(3,"C Programming Language" )
+        };
+        public Book[] GetAllByTitle(string titlepart)
+        {
+           return books.Where(book => book.Title.Contains(titlepart)).ToArray();
+        }
+    }
+}
